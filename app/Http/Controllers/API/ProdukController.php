0d87ebfd9c953927274012b730d9a5db90d60163
Dika Alfarell <al.ramdhan312@gmail.com>
2026-01-16 23:15:49 +0700

    update get kategori

--- app/Http/Controllers/API/ProdukController.php
+++ app/Http/Controllers/API/ProdukController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\API;
 
+use Throwable;
 use App\Models\Produk;
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
 
@@ -10,16 +12,29 @@ class ProdukController extends BaseController
 {
     public function getProduk()
     {
-        $query = Produk::with('kategori')->where('is_active', true)
-            ->where('stok', '>', 0)->get();
-
-        return $this->sendResponse([
-            'data' => $query
-        ], 'Data berhasil diambil');
+        try {
+            $query = Produk::with('kategori')->where('is_active', true)
+                ->where('stok', '>', 0)->get();
+    
+            return $this->sendResponse([
+                'data' => $query
+            ], 'Data berhasil diambil');
+        } catch (Throwable $e) {
+            return $this->sendError();
+        }
     }
 
     public function getKategoriProduk()
     {
+        try {
+            $query = Kategori::where('is_active', true)
+                ->get();
 
+            return $this->sendResponse([
+                'data' => $query
+            ], 'Data berhasil diambil');
+        } catch(Throwable $e) {
+            return $this->sendError('gagal');
+        }
     }
 }
---
+++ database/seeders/ProdukTableSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Kategori;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ProdukTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $dataKategori = [
+            [
+                'name' => 'Makanan',
+                'image' => asset('storage/image-kategori/makanan.png')
+            ],
+            [
+                'name' => 'Minuman',
+                'image' => asset('storage/image-kategori/minuman.png')
+            ],
+            [
+                'name' => 'Appetizer',
+                'image' => asset('storage/image-kategori/appetizer.png')
+            ],
+            [
+                'name' => 'Dessert',
+                'image' => asset('storage/image-kategori/dessert.png')
+            ],
+            [
+                'name' => 'Kids Menu',
+                'image' => asset('storage/image-kategori/kids-menu.png')
+            ],
+            // [
+            //     'name' => 'Esspresso',
+            //     'image' => asset('storage/image-kategori/esspresso.png')
+            // ],
+            // [
+            //     'name' => 'Manual Brew',
+            //     'image' => asset('storage/image-kategori/manual-brew.png')
+            // ],
+            // [
+            //     'name' => 'Signature Coffee',
+            //     'image' => asset('storage/image-kategori/signature-coffee.png')
+            // ],
+            // [
+            //     'name' => 'Bottled Coffee',
+            //     'image' => asset('storage/image-kategori/bottled-coffee.png')
+            // ],
+        ];
+        $produks = [
+            [
+                'kategori_id' => 1,
+                'nama_produk' => '',
+            ]
+        ];
+
+        foreach($dataKategori as $key => $kategori) {
+            Kategori::create([
+                'name' => $kategori['name'],
+                'image' => $kategori['image'],
+                'sort_order' => ($key+1)
+            ]);
+        }
+    }
+}
